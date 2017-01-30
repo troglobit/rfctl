8603a367332979225ad62afd57fb7b8ab168a2a1
Joachim Nilsson <troglobit@gmail.com>
2017-01-30 07:45:54 +0100

    Update GPL v2 license, new address etc.
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,11 +1,22 @@
 /* RF bitbang control tool for NEXA and other RF remote receivers
  *
- * This tool uses the RF bitbanger interface provided by the rfbb.ko
- * kernel driver.  Some code is borrowed from rfcmd.
- *
  * Copyright (C) 2010 Tord Andersson <tord.andersson@endian.se>
  *
- * License: GPL v. 2
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, visit the Free Software Foundation
+ * website at http://www.gnu.org/licenses/gpl-2.0.html or write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 #include <stdio.h>
